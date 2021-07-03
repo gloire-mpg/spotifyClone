7fc50dd019c97bba3345c960d8b4da7e908ee4ee
gloire-mpg <69449719+gloire-mpg@users.noreply.github.com>
2021-07-03 17:57:16 +0530

    Updates

---
+++ precache-manifest.76319f5d1fccf356fd470e2f7c58d57d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "559e6b2d1cdfd38e2b0062416e372b18",
+    "url": "/spotifyClone/index.html"
+  },
+  {
+    "revision": "90139701bfe891dc953d",
+    "url": "/spotifyClone/static/css/main.3d7ed0f0.chunk.css"
+  },
+  {
+    "revision": "aa5297dcfbf498dd1078",
+    "url": "/spotifyClone/static/js/2.eced1c94.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/spotifyClone/static/js/2.eced1c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90139701bfe891dc953d",
+    "url": "/spotifyClone/static/js/main.92451a33.chunk.js"
+  },
+  {
+    "revision": "a58f420d608160614b5a",
+    "url": "/spotifyClone/static/js/runtime-main.ed2ff9d5.js"
+  }
+]);
